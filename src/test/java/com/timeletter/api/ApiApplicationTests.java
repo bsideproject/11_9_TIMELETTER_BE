cc1178a4766ea97ed81a0d25d2473fdc1ec1bb84
You Dong Kwan <dkyou7@naver.com>
2022-07-15 14:28:32 +0900

    Init project

---
+++ src/test/java/com/timeletter/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeletter.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
