517d621ee360654d7fe39025c836d31b02998078
youdongkwan <dkyou7@naver.com>
2022-07-16 15:10:39 +0900

    feat: LetterRepository.java 작성

--- src/main/java/com/timeletter/api/letter/LetterRepository.java
+++ src/main/java/com/timeletter/api/letter/LetterRepository.java
@@ -1,4 +1,8 @@
 package com.timeletter.api.letter;
 
-public interface LetterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LetterRepository extends JpaRepository<Letter,String> {
 }
