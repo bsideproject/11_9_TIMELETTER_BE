e1c71c79555ca88ebb436fa7ad84bd32e79dd403
youdongkwan <dkyou7@naver.com>
2022-07-19 00:55:21 +0900

    feat: create entity

--- src/main/java/com/timeletter/api/letter/LetterControllerAPI.java
+++ src/main/java/com/timeletter/api/letter/LetterControllerAPI.java
@@ -1,4 +1,16 @@
 package com.timeletter.api.letter;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/v1/letter")
 public class LetterControllerAPI {
+
+    private final LetterService letterService;
 }
--- src/main/java/com/timeletter/api/letter/LetterService.java
+++ src/main/java/com/timeletter/api/letter/LetterService.java
@@ -1,4 +1,34 @@
 package com.timeletter.api.letter;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class LetterService {
+
+    private final LetterRepository letterRepository;
+
+    public Letter create(final Letter entity){
+        validate(entity);
+
+        letterRepository.save(entity);
+
+        log.info("Entity id : {} is saved",entity.getId());
+
+        return letterRepository.findById(entity.getId()).get();
+    }
+
+    private void validate(Letter entity) {
+        if(entity == null){
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+    }
 }
