3a1d1c3c9cb2ff15c014e8acf0596da6a8be918a
youdongkwan <dkyou7@naver.com>
2022-07-24 17:45:18 +0900

    feat: Member.java init

---
+++ src/main/java/com/timeletter/api/member/Member.java
@@ -0,0 +1,31 @@
+package com.timeletter.api.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class Member {
+
+    @Id @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid",strategy = "uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
