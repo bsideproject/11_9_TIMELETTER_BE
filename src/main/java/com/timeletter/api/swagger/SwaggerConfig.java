4ec1a147a25b0dbfd625c60cbc17a82ba5dd2833
You Dong Kwan <dkyou7@naver.com>
2022-08-18 13:06:24 +0900

    feat: Swagger config 설정 변경

--- src/main/java/com/timeletter/api/swagger/SwaggerConfig.java
+++ src/main/java/com/timeletter/api/swagger/SwaggerConfig.java
@@ -1,35 +1,48 @@
 package com.timeletter.api.swagger;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
-@EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
-
     @Bean
-    public Docket restAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
-        	.useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.timeletter.api"))
                 .paths(PathSelectors.any())
+                .build()
+                ;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
                 .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("TimeLetter API")
-                .version("1.0.0")
-                .description("타임레터 API 입니다.")
-                .build();
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
