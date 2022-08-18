3b006d7f99548a3018e46ce8fb4536006c38aacb
You Dong Kwan <dkyou7@naver.com>
2022-08-18 13:59:25 +0900

    refactor: 주석 작성 및 메소드 분리 작업

--- src/main/java/com/timeletter/api/security/TokenProvider.java
+++ src/main/java/com/timeletter/api/security/TokenProvider.java
@@ -3,7 +3,6 @@ package com.timeletter.api.security;
 import com.timeletter.api.member.Member;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
@@ -12,34 +11,68 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 @Slf4j
 @Service
 public class TokenProvider {
     private static final String SECRET_KEY = "Ns43fsd233MM9F05fNs43fsd233MM9F05fNs43fsd233MM9F05fNs43fsd233MM9F05f";
+    private static final int amountToAdd = 100;
+    private static final TemporalUnit unit = ChronoUnit.DAYS;
 
+    /**
+     * 토큰 생성
+     *
+     * @param member 사용자 정보
+     * @return 토큰 발행한 것
+     */
     public String create(Member member){
-        Date expiryDate = Date.from(Instant.now().plus(3, ChronoUnit.MONTHS));
+        Date expiryDate = getExpiryDate();
+        Key key = getKey();
 
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        Key key = Keys.hmacShaKeyFor(keyBytes);
-
-        return Jwts.builder().signWith(key)
-                .setSubject(member.getId())
+        return Jwts.builder()
+                .signWith(key)
+                .setSubject(member.getEmail())
                 .setIssuer("TimeLetter api")
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
                 .compact();
     }
 
+    /**
+     * 토큰 만료 일자 설정하기
+     *
+     * @return 토큰 만료 일자
+     */
+    private Date getExpiryDate() {
+        return Date.from(Instant.now().plus(amountToAdd, unit));
+    }
+
+    /**
+     * 토큰정보를 바탕으로 이메일을 반환한다.
+     *
+     * @param token 토큰 정보
+     * @return 이메일
+     */
     public String validateAndGetUserID(String token){
+        Key key = getKey();
+
         Claims claims = Jwts.parserBuilder()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
 
         return claims.getSubject();
+    }
+
+    /**
+     * 키를 가져온다.
+     *
+     * @return 시크릿 키를 암호화한 값
+     */
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
     }
 }
