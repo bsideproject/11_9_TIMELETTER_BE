725f824544cf397d0ab34adef765151aab83301c
You Dong Kwan <dkyou7@naver.com>
2022-08-18 15:54:40 +0900

    refactor: swaggerAPI 관련 설정 변경

--- src/main/java/com/timeletter/api/swagger/SwaggerConfig.java
+++ src/main/java/com/timeletter/api/swagger/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.timeletter.api.swagger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
@@ -20,6 +22,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .securityContexts(List.of(securityContext()))
                 .securitySchemes(List.of(apiKey()))
                 .select()
@@ -44,5 +47,13 @@ public class SwaggerConfig {
 
     private ApiKey apiKey() {
         return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("TimeLetter API")
+                .version("1.0.0")
+                .description("타임레터 API 입니다.")
+                .build();
     }
 }
