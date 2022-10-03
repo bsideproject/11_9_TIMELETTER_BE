bb7d7cd236f0ac2938d74ac93459c046d55dfacf
JeongMin Woo <minhan_a@naver.com>
2022-10-03 21:13:26 +0900

    Update Letter.java

--- src/main/java/com/timeletter/api/letter/Letter.java
+++ src/main/java/com/timeletter/api/letter/Letter.java
@@ -28,10 +28,13 @@ public class Letter {
     // private String title; // 편지 제목
     @Column(columnDefinition = "TEXT")
     private String content; // 편지 내용
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime receivedDate; // 받을 날짜 지정
     // private String receivedPhoneNumber; // 받을 사람 휴대폰 번호 지정
 
     @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt; // 생성 시간 지정
 
