4352427b6d9b7a33cdaffc189042ebcf1b0a5619
whoo3474 <minhan_a@naver.com>
2022-10-03 18:47:08 +0900

    urlslug 길이 수정

--- src/main/java/com/timeletter/api/letter/LetterService.java
+++ src/main/java/com/timeletter/api/letter/LetterService.java
@@ -173,7 +173,7 @@ public class LetterService {
 
             int leftLimit = 48; // numeral '0'
             int rightLimit = 122; // letter 'z'
-            int targetStringLength = 20;
+            int targetStringLength = 32;
             Random random = new Random();
             String generatedString = random.ints(leftLimit, rightLimit + 1)
                     .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
